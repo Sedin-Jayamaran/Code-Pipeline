e999129fe5115b7eb4a4928da0e7c9137cc63aee
jayamaran <jayamaran@sedintechnologies.com>
2025-08-31 17:28:44 +0000

    sss

--- Buggy-App/Terraform/variable.tf
+++ Buggy-App/Terraform/variable.tf
@@ -6,3 +6,8 @@ variable "image" {
   description = "Docker image to use for ECS task"
   type        = string
 }
+
+variable "service_name" {
+  description = "Name of the ECS service"
+  type        = string
+}
