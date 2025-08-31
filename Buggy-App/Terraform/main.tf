bdbcd3e4ae9ecb28f77d103389245069b18cc7fd
jayamaran <jayamaran@sedintechnologies.com>
2025-08-31 15:36:31 +0000

    new

--- Buggy-App/Terraform/main.tf
+++ Buggy-App/Terraform/main.tf
@@ -1,10 +1,11 @@
-module "service" {
-    source = "./service"
-    td_arn = module.taskdefinition.task_definition_arn
-    
-}
 module "taskdefinition" {
     source = "./taskdefinition"
     image = var.image 
 
+}
+
+module "service" {
+    source = "./service"
+    td_arn = module.taskdefinition.task_definition_arn
+    
 }
