1979659e18326d6541a2e63021f566dfd3276c16
jayamaran <jayamaran@sedintechnologies.com>
2025-09-04 07:39:15 +0000

    db

--- Buggy-App/scripts/entrypoint.sh
+++ Buggy-App/scripts/entrypoint.sh
@@ -4,9 +4,6 @@ set -e
 # ensure gems are up to date
 bundle check || bundle install
 
-# Optional: run DB migrations
-echo "Running migrations..."
-bundle exec rake db:migrate
 
 # Precompile assets (if not already done in Dockerfile)
 echo "Precompiling assets..."
